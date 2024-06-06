ee403c591ebb1762ed6be1467d62f5a62d343d5b
Espyz <truharandrey1015@gmail.com>
2024-06-06 10:13:15 +0500

    Сделал Множественные схемы

--- back.py
+++ back.py
@@ -1,11 +1,13 @@
 import json
 import sys
+import os
 from flask import Flask, request
 app = Flask(__name__)
 start_id = str(sys.maxsize * 2 + 1)
+last_schema = len(os.listdir(path='Schemes')) + 1
 @app.route('/schema', methods=['POST'])
 def scheme_handler():
-    global start_id
+    global start_id, last_schema
     node_list = {}
     schema = request.get_json()['schema']
     nodes = schema['nodes']
@@ -19,7 +21,8 @@ def scheme_handler():
     for edge in edges:
         node_list[edge['source']]['childs'].append(edge['target'])
     
-    file = open('schemes.json', 'w', encoding='utf-8')
+    file = open(f'Schemes/schema{ last_schema }.json', 'w', encoding='utf-8')
+    last_schema += 1
     json.dump(node_list, file, ensure_ascii=False)
     file.close()
     return { 'message': 'success', 'status_code': 200 }
--- main.py
+++ main.py
@@ -1,29 +1,27 @@
 import json
-import sys
+import os
 import telebot
 from telebot import types
 bot = telebot.TeleBot('7230363856:AAEWb1BVKPeTgtw-ol-5rduEkGe_jJm5eVo')
 
-node_list = {}
-start_id = str(sys.maxsize * 2 + 1)
-
-def keyboard_generate(id):
+def keyboard_generate(id, schema_id, node_list):
     keyboard = types.InlineKeyboardMarkup()
     for child in node_list[id]['childs']:
-        button = types.InlineKeyboardButton(text=node_list[child]['label'], callback_data=child)
+        data = f'{child} {schema_id}'
+        button = types.InlineKeyboardButton(text=node_list[child]['label'], callback_data=data)
         keyboard.add(button)
     return keyboard
 
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    global node_list
     if message.text == '/start':
-        file = open('schemes.json', 'r', encoding='utf-8')
+        last_schema = len(os.listdir(path='Schemes'))
+        print(last_schema)
+        file = open(f'Schemes/schema{last_schema}.json', 'r', encoding='utf-8')
         node_list = json.load(file)
         file.close()
         start_id = min(list(node_list.keys()))
-        print(node_list)
         # Создание меню с командами
         bot.set_my_commands(
             commands=[
@@ -31,19 +29,25 @@ def get_text_messages(message):
             ],
             scope=types.BotCommandScopeChat(message.chat.id)
         )
-        keyboard = keyboard_generate(start_id)
+        keyboard = keyboard_generate(start_id, last_schema, node_list)
         bot.send_message(message.from_user.id, text=node_list[start_id]['label'], reply_markup=keyboard)
     else:
         bot.send_message(message.from_user.id, text='Неизвестная команда, введите /start для начала работы')
 
 
-@bot.callback_query_handler(func=lambda call: True)
+@bot.callback_query_handler(func=lambda call: True, )
 def callback_worker(call):
-    if (len(node_list[call.data]['childs']) > 0):
-        keyboard = keyboard_generate(node_list[call.data]['childs'][0])
-        bot.send_message(call.message.chat.id, text=node_list[node_list[call.data]['childs'][0]]['label'], reply_markup=keyboard)
+    [ node_id, schema_id ] = call.data.split(' ')
+
+    file = open(f'Schemes/schema{schema_id}.json', 'r', encoding='utf-8')
+    node_list = json.load(file)
+    file.close()
+
+    if (len(node_list[node_id]['childs']) > 0):
+        keyboard = keyboard_generate(node_list[node_id]['childs'][0], schema_id, node_list)
+        bot.send_message(call.message.chat.id, text=node_list[node_list[node_id]['childs'][0]]['label'], reply_markup=keyboard)
     else:
-        bot.send_message(call.message.chat.id, text=node_list[call.data]['label'])
+        bot.send_message(call.message.chat.id, text=node_list[node_id]['label'])
 
 
 bot.polling(none_stop=True, interval=0)
